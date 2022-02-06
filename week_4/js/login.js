6fe2a4774622c929bc311036fc32e4036cf1a9b5
Cong yum <locryacoyu@hotmail.com>
2022-02-06 15:42:25 +0800

    loginIsOk

--- week_4/js/login.js
+++ week_4/js/login.js
@@ -14,13 +14,14 @@ const app = Vue.createApp({
     methods: {
         login(){
             console.log(this.user);
-            // axios.get('')
-            //     .then(res=>{
+            axios.post(`${this.api_url}/admin/signin`, this.user)
+                .then(res=>{
+                    console.log(res.data.success);
+                    res.data.success ? window.location.replace('./products.html') : false ;
+                })
+                .catch(err=>{
 
-            //     })
-            //     .catch(err=>{
-
-            //     })
+                })
         }
     },
     mounted() {
